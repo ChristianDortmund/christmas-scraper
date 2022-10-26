7657404c62e5897e4f8e1627ff1c60d2cec66fb6
ChristianDortmund <cmechow@mailbox.org>
2022-10-26 06:26:37 +0200

    start scraping

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-import {readFilesync} from 'fs';
-import {join} from 'path';
-
 const addButton = document.getElementById('addMovie');
 const movieList = [];
 const liste = document.getElementById('liste');
 
 addButton.addEventListener("click", () => {
-    const filmlistJson = readFilesync(join(__dirname, 'filmlist.json'));
-    console.log(typeof filmlistJson);
     let film =  document.getElementById('film');
+
     if (movieList.includes(film.value)) {
         return alert('Film bereits enthalten!');
-    }
+    };
+
     let item = document.createElement("li");
     let hidden = document.createElement("input");
+
     hidden.setAttribute("type", "hidden");
     hidden.setAttribute("name", "filme[]");
     hidden.setAttribute("value", film.value);
+
     liste.appendChild(hidden);
     item.innerText = film.value;
+
     liste.appendChild(item);
+
     movieList.push(film.value);
+    
     film.value = '';
  });
--- routes/scrape.js
+++ routes/scrape.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const got = require('got');
+const cheerio = require('cheerio');
+let date = '2022-10-26';
+const url = `https://www.tvspielfilm.de/tv-programm/tv-sender/?page=1&date=${date}&slots=20`;
 
 const path = require('path');
 const fs = require('fs');
@@ -7,7 +11,7 @@ const fs = require('fs');
 router.get('/search-result', (req, res, next) => {
     res.send('<h1>worked</h1>');
     const filmlistJson = fs.readFileSync(path.join(__dirname, '../', 'filmlist.json'), "utf8");
-    console.log(filmlistJson);
+    console.log(url);
 });
 
 module.exports = router;
